41decc0fae2d574283525adb4677ff7e248aaad6
brandylyn <brandylynlanders@gmail.com>
2023-02-11 11:47:08 -0600

    on number 7

--- scripts/movie joins.sql
+++ scripts/movie joins.sql
@@ -39,15 +39,32 @@ GROUP BY distributors.company_name
 ORDER BY COUNT(specs.movie_id); 
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
-SELECT DISTINCT distributors.company_name, revenue.film_budget
+SELECT distributors.company_name, ROUND(AVG(revenue.film_budget),2)AS avg_budget
+FROM distributors 
+INNER JOIN specs 
+ON distributors.distributor_id=specs.domestic_distributor_id
+INNER JOIN revenue
+ON specs.movie_id=revenue.movie_id
+GROUP BY distributors.company_name
+ORDER BY AVG(revenue.film_budget) DESC
+LIMIT 5;
+
+-- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating? 
+SELECT company_name, film_title, worldwide_gross
 FROM distributors
 INNER JOIN specs 
 ON distributors.distributor_id=specs.domestic_distributor_id
 INNER JOIN revenue
 ON specs.movie_id=revenue.movie_id
-ORDER BY film_budget DESC
-LIMIT 5;
-
--- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating? 
+WHERE specs.mpaa_rating = 'G'
+ORDER BY revenue.worldwide_gross DESC;
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+SELECT company_name, film_title, worldwide_gross
+FROM distributors
+INNER JOIN specs 
+ON distributors.distributor_id=specs.domestic_distributor_id
+INNER JOIN revenue
+ON specs.movie_id=revenue.movie_id
+WHERE specs.mpaa_rating = 'G'
+ORDER BY revenue.worldwide_gross DESC;
